66b9efb58a01f87299db95dcebc8803cae883033
FrancoisBruere <85226595+FrancoisBruere@users.noreply.github.com>
2021-07-28 16:22:53 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CalculateAge
+{
+    public class Person
+    {
+
+        public DateTime Birthdate { get; private set; }
+
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+        public int Age
+        {
+            get
+            {
+                var timespan = DateTime.Now - Birthdate;
+                var years = timespan.Days / 365;
+
+                return years;
+            }
+          
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CalculateAge
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var person = new Person(new DateTime(1976,11,19));
+
+            //person.Birthdate = (new DateTime(1976, 11, 19));
+
+            Console.WriteLine("I am {0} years old!",person.Age);
+
+        }
+    }
+}
